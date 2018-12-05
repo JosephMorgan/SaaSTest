633942f3e54329b626a9441d29f123f2b42f6e29
JosephMorgan <bin.deng91@gmail.com>
2018-12-05 21:18:36 +0800

    capture screen use robot

--- src/test/java/com/beeboxes/face/util/DotTestListener.java
+++ src/test/java/com/beeboxes/face/util/DotTestListener.java
@@ -3,8 +3,6 @@ package com.beeboxes.face.util;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.TestListenerAdapter;
@@ -19,12 +17,13 @@ public class DotTestListener extends TestListenerAdapter {
 	@Override
 	public void onTestFailure(ITestResult tr) {
 		try {
-			TestBase tb = (TestBase) tr.getInstance();
-			WebDriver driver = tb.getDriver();
+			//TestBase tb = (TestBase) tr.getInstance();
+			//WebDriver driver = tb.getDriver();
 			 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");  //转换时间格式
 			 String time = dateFormat.format(Calendar.getInstance().getTime());  //获取当前时间
 			Reporter.log("出错时间："+time);
-			Screenshot.snapshot((TakesScreenshot)driver);
+			//Screenshot.snapshot((TakesScreenshot)driver);
+			Screenshot.captureScreen();
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
--- src/test/java/com/beeboxes/face/util/Screenshot.java
+++ src/test/java/com/beeboxes/face/util/Screenshot.java
@@ -1,8 +1,17 @@
 package com.beeboxes.face.util;
 
+import java.awt.AWTException;
+import java.awt.HeadlessException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -16,9 +25,9 @@ import org.openqa.selenium.TakesScreenshot;
  */
 public class Screenshot {
 	
-	/** 截的图带名字fileName的png图片*/
+	/** 截的图带名字fileName的png图片。*/
 	public static void snapshot(TakesScreenshot drivername,String fileName) {
-		 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");  //转换时间格式
+		 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");  
 		 String time = dateFormat.format(Calendar.getInstance().getTime());  //获取当前时间
 	     File srcFile = drivername.getScreenshotAs(OutputType.FILE);
 		  try {
@@ -28,7 +37,7 @@ public class Screenshot {
 		}
 	  }
 	
-	/** 截的图的名字是failure时间戳png*/
+	/** 截的图的名字是failure时间戳png。*/
 	public static void snapshot(TakesScreenshot drivername) {
 		 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");  //转换时间格式yyyyMMddHHmmss
 		 String time = dateFormat.format(Calendar.getInstance().getTime());  //获取当前时间
@@ -39,5 +48,21 @@ public class Screenshot {
 			e.printStackTrace();
 		}
 	  }
-
+	
+	/**用Robot这个类截图。原理大概是，从电脑屏幕左上角画一个长方形，一直画到屏幕右下角，
+	 * 然后得到File对象，在把这个File是用png或jpg保存，复制到一个具体路径，这个就是截图的整个流程。*/
+	public static void captureScreen()  {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+		String time = dateFormat.format(Calendar.getInstance().getTime());  //获取当前时间
+		BufferedImage image;
+		try {
+			image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+			ImageIO.write(image, "png", new File("snapshot","failure"+time+".png")); 
+		} catch (HeadlessException | AWTException e) {
+			e.printStackTrace();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 }
